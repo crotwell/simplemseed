d719b9c8ec84a6febb8d619b0240df617a66043f
Philip Crotwell <crotwell@seis.sc.edu>
2024-02-28 11:50:11 -0500

    basic readthedocs stuff, seems to work locally

---
+++ docs/source/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# set up path to find source code
+import os, sys
+
+sys.path.append(os.path.abspath('../../src'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SimpleMSeed'
+copyright = '2024, Philip Crotwell'
+author = 'Philip Crotwell'
+release = '0.2'
+version = '0.2.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    "autodoc2",
+    'sphinx.ext.intersphinx',
+    'myst_parser',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+autodoc2_packages = [
+    "../../src/simplemseed",
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ docs/source/index.rst
@@ -0,0 +1,26 @@
+.. simplemseed documentation master file, created by
+   sphinx-quickstart on Wed Feb 28 09:34:51 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+SimpleMSeed
+===========
+
+
+`Miniseed3 <http://docs.fdsn.org/projects/miniseed3>`_ (and reading 2) in pure python.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   README <readme_link>
+   apidocs/index
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
