5cc1984ccb5e871664deec2cb07383f1d9f9ff6f
Philip Crotwell <crotwell@seis.sc.edu>
2024-08-14 14:14:05 -0400

    ver to 0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,8 +10,8 @@
 project = 'SimpleMSeed'
 copyright = '2024, Philip Crotwell'
 author = 'Philip Crotwell'
-release = '0.3'
-version = '0.3.1'
+release = '0.4'
+version = '0.4.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
