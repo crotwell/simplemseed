ddc09f01883a2a0a233418e045d977f2cc64accf
Philip Crotwell <crotwell@seis.sc.edu>
2025-02-19 20:15:30 -0500

    test read via file ptr loop

--- tests/test_mseed2.py
+++ tests/test_mseed2.py
@@ -18,8 +18,41 @@ TEST_DIR = Path(__file__).parent
 # https://service.iris.edu/fdsnws/dataselect/1/query?net=CO&sta=CASEE&loc=00&cha=HHZ&starttime=2023-06-17T04:53:54.468&endtime=2023-06-17T04:55:00&format=miniseed&nodata=404
 #
 
+casee_msi_data = [89, 67, 53, 71, 86, 89,
+    97, 96, 81, 90, 94, 73,
+    73, 79, 87, 100, 91, 107,
+   105, 102, 112, 93, 106, 101,
+    92, 100, 84, 99, 97, 108,
+   151, 130, 114, 124, 116, 116,
+   102, 108, 130, 121, 127, 131,
+   129, 134, 109, 112, 123, 121,
+   139, 132, 153, 157, 128, 140,
+   129, 140, 150, 138, 158, 141,
+   132, 137, 131, 149, 159, 156,
+   142, 140, 158, 154, 149, 141,
+   135, 152, 152, 157, 168, 162,
+   158, 151, 144, 148, 137, 133,
+   147, 150, 155, 139, 134, 154,
+   149, 156, 152, 137, 142, 145,
+   147, 142, 138, 143, 136, 140,
+   143, 137]
+
 
 class TestMseed2:
+    def test_read_casee_fileptr(self):
+        with open(TEST_DIR / "casee.mseed2", "rb") as f:
+            for rec in simplemseed.miniseed.readMiniseed2Records(f):
+                assert rec.codes() == "CO.CASEE.00.HHZ"
+                assert rec.starttime() == datetime.fromisoformat(
+                    "2023-06-17T04:53:54.468648+00:00"
+                )
+                data = rec.decompress()
+                assert len(data) == len(
+                    casee_msi_data
+                ), f"d:{len(data)} should be orig:{len(casee_msi_data)}"
+                for i in range(len(casee_msi_data)):
+                    assert casee_msi_data[i] == data[i]
+
     def test_read(self):
         with open(TEST_DIR / "casee.mseed2", "rb") as f:
             rec_bytes = f.read()
@@ -31,24 +64,6 @@ class TestMseed2:
             )
             data = rec.decompressed()
 
-            msi_data = [89, 67, 53, 71, 86, 89,
-                97, 96, 81, 90, 94, 73,
-                73, 79, 87, 100, 91, 107,
-               105, 102, 112, 93, 106, 101,
-                92, 100, 84, 99, 97, 108,
-               151, 130, 114, 124, 116, 116,
-               102, 108, 130, 121, 127, 131,
-               129, 134, 109, 112, 123, 121,
-               139, 132, 153, 157, 128, 140,
-               129, 140, 150, 138, 158, 141,
-               132, 137, 131, 149, 159, 156,
-               142, 140, 158, 154, 149, 141,
-               135, 152, 152, 157, 168, 162,
-               158, 151, 144, 148, 137, 133,
-               147, 150, 155, 139, 134, 154,
-               149, 156, 152, 137, 142, 145,
-               147, 142, 138, 143, 136, 140,
-               143, 137]
             # vals from msi -d -n 1 casee.mseed2
             assert data[0] == 89
             assert data[1] == 67
@@ -57,10 +72,10 @@ class TestMseed2:
             assert data[4] == 86
             assert data[5] == 89
             assert len(data) == len(
-                msi_data
-            ), f"d:{len(data)} should be orig:{len(msi_data)}"
-            for i in range(len(msi_data)):
-                assert msi_data[i] == data[i]
+                casee_msi_data
+            ), f"d:{len(data)} should be orig:{len(casee_msi_data)}"
+            for i in range(len(casee_msi_data)):
+                assert casee_msi_data[i] == data[i]
 
     def test_read_dataless(self):
         filename = TEST_DIR / "IU_PET_00_A_C_E.mseed2"
