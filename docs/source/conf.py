90c8389cb95d1bfe38ef60ca071e64ebce9bcee4
Philip Crotwell <crotwell@seis.sc.edu>
2025-09-12 10:40:06 -0400

    importlib not needed if python>3.10, ver to 0.5.1-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ project = 'SimpleMSeed'
 copyright = '2025, Philip Crotwell'
 author = 'Philip Crotwell'
 release = '0.5'
-version = '0.5.0'
+version = '0.5.1-dev'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
